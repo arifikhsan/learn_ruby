0ac2d413e943fb5219398ee48c6f87551bfa4e78
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-03-10 23:15:17 +0700

    simon says...

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,37 @@
 #write your code here
+
+def add num_one, num_two
+    num_one + num_two
+end
+
+def subtract num_one, num_two
+    num_one - num_two
+end
+
+def sum numbers
+    sum = 0
+    numbers.each do |number|
+        sum += number
+    end
+    sum
+end
+
+def multiply x, y
+    x * y
+end
+
+def multiplies numbers
+    numbers.reject(&:zero?).inject(:*)
+end
+
+def power number, of
+    number ** of
+end
+
+def factorial number
+    if number <= 1
+        1
+    else
+        number * factorial(number - 1)
+    end
+end
